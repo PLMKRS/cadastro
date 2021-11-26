bfa2cbcc5429f7eaed9b9a713374fae000561efa
Virtus <a.yuri@yahoo.com.br>
2021-11-26 17:39:08 -0300

    commit final

--- pessoa/admin.py
+++ pessoa/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import display
+from .models import Pessoa, Contato
 
-# Register your models here.
+
+# A rotina abaixo cria os campos na tabela do banco Django-Admin no front-end.
+
+@admin.action(description='Ativar todas as Pessoas')
+def ativar_todos(modeladmin, request, queryset):
+    queryset.update(ativo=True)
+
+
+@admin.action(description='Desativar todas as Pessoas')
+def desativar_todos(modeladmin, request, queryset):
+    queryset.update(ativo=False)
+
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome_completo',
+        'data_nascimento',
+        'ativo'
+    ]
+    list_filter = [
+        'ativo',
+        'data_nascimento'
+    ]
+    search_fields = [
+        'nome_completo'
+    ]
+    actions = [
+        ativar_todos,
+        desativar_todos
+    ]
+
+
+admin.site.register(Pessoa, PessoaAdmin)
+admin.site.register(Contato)
--- project/settings.py
+++ project/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -129,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = '/'
